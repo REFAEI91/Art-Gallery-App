e8b9b0301a5ff4ee3e01e99fe17957c1ef421b84
doctype-Melvin <doctype.melvin@gmail.com>
2023-04-21 08:41:38 +0200

    removes muted code

--- components/ArtPiecePreview.js
+++ components/ArtPiecePreview.js
@@ -23,10 +23,6 @@ export function ArtPiecePreview({ image, name, artist, slug }) {
   return (
     <div
       style={{
-        // display: "grid",
-        // gridTemplateColumns: "repeat(auto-fit, minmax(500px, 1fr))",
-        // gridGap: "1rem",
-        // justifyItems: "center",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
