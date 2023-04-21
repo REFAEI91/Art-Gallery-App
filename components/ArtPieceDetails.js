a3f8e0eb9f28a06d087b86890d34819b4b0fe271
M.Refaei Hannan <56527662+REFAEI91@users.noreply.github.com>
2023-04-21 11:18:30 +0200

    FavoriteButton

--- components/ArtPieceDetails.js
+++ components/ArtPieceDetails.js
@@ -1,44 +1,60 @@
 import Image from "next/image";
 import styled from "styled-components";
 import Link from "next/link";
+import FavoriteButton from "./FavoriteButton";
 
-
-export default function ArtPieceDetails({ image, title, artist, year, genre, artPiece }) {
-
+export default function ArtPieceDetails({
+  image,
+  title,
+  artist,
+  year,
+  genre,
+  artPiece,
+}) {
   const PrettyView = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 5vw 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 5vw 0;
 
-  .artwork {
-    object-fit: contain;
-  }
-  `
+    .artwork {
+      object-fit: contain;
+    }
+  `;
 
   const BackButton = styled.button`
-  background: transparent;
-  border: solid 1px #000;
-  color: #000;
-  border-radius: 15px;
-  padding: 5px 10px;
+    background: transparent;
+    border: solid 1px #000;
+    color: #000;
+    border-radius: 15px;
+    padding: 5px 10px;
 
-  &:hover {
-    cursor: pointer;
-    background-color: #fef;
-  }
-  `
+    &:hover {
+      cursor: pointer;
+      background-color: #fef;
+    }
+  `;
 
   return (
     <PrettyView>
-      <Image src={image} alt={title} width={artPiece.dimensions.width/3} height={artPiece.dimensions.height/3} className="artwork" />
+      <FavoriteButton
+        isFavorite={artPiece.favorite}
+        onToggleFavorite={() => {}}
+      />
+      <Image
+        src={image}
+        alt={title}
+        width={artPiece.dimensions.width / 3}
+        height={artPiece.dimensions.height / 3}
+        className="artwork"
+      />
       <h2>Title: {title}</h2>
       <h3>Artist: {artist} </h3>
       <p>Year: {year} </p>
       <p>{genre} </p>
-    <Link href="/art-pieces">
-    <BackButton type="button">Back to gallery</BackButton>
+      <Link href="/art-pieces">
+        <BackButton type="button">Back to gallery</BackButton>
       </Link>
     </PrettyView>
   );
---
+++ components/FavoriteButton.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+export default function FavoriteButton({ isFavorite, onToggleFavorite }) {
+  const FavoriteButton = styled.button`
+    background: transparent;
+    border: solid 1px #000;
+    color: #000;
+    border-radius: 15px;
+    padding: 5px 10px;
+    display: flex;
+    position: absolute;
+    top: 10%;
+    right: 5%;
+  `;
+
+  return (
+    <FavoriteButton onClick={onToggleFavorite}>
+      {isFavorite ? "Remove from favorites" : "Add to favorites"}
+    </FavoriteButton>
+  );
+}
