da31a8b6301ff110a76434720c92fa211fb82f36
Melvin Speckamp <doctype.melvin@gmail.com>
2023-04-21 09:44:33 +0200

    start userStory5

--- components/Spotlight.js
+++ components/Spotlight.js
@@ -9,26 +9,28 @@ function randomNumber(array) {
 
 export default function Spotlight({ pieces }) {
   const [randomArtpiece, setRandomArtpiece] = useState(randomNumber(pieces));
-  console.log(randomArtpiece);
-  const StyledSpotlight = styled.div`
-  margin: 0 auto;
-  height: 100vh;
-  width: 60vw;
-  text-align: center;
-  `
 
+  const StyledSpotlight = styled.div`
+    margin: 0 auto;
+    height: 100vh;
+    width: 60vw;
+    text-align: center;
+  `;
 
   return (
     <StyledSpotlight>
       <h1>Spotlight</h1>
       <Link href={`/art-pieces/${randomArtpiece.slug}`}>
-      <Image 
-      src={randomArtpiece.imageSource} 
-      alt={randomArtpiece.name} 
-      width={randomArtpiece.dimensions.width/3}
-      height={randomArtpiece.dimensions.height/3}
-      style={{objectFit: "contain", boxShadow: "1px 1px 10px 2px rgba(0, 0, 0, 0.5)"}}
-      />
+        <Image
+          src={randomArtpiece.imageSource}
+          alt={randomArtpiece.name}
+          width={randomArtpiece.dimensions.width / 3}
+          height={randomArtpiece.dimensions.height / 3}
+          style={{
+            objectFit: "contain",
+            boxShadow: "1px 1px 10px 2px rgba(0, 0, 0, 0.5)",
+          }}
+        />
       </Link>
       <p>{randomArtpiece.name}</p>
     </StyledSpotlight>
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
 import GlobalStyle from "../styles";
 import useSWR from "swr";
 import Layout from "@/components/Layout";
+import { useState } from "react";
 
 const URL = "https://example-apis.vercel.app/api/art";
 const fetcher = (URL) => fetch(URL).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
+  const [artPiecesInfo, setArtPiecesInfo] = useState({});
+
   const { data, error, isLoading } = useSWR(URL, fetcher);
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error</div>;
   if (!data) return <div>No data</div>;
+
   return (
     <Layout>
       <GlobalStyle />
